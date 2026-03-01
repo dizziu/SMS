761db3b5cb4418a0af420279f3e38f14a51ec5c4
dizziu <104685792+dizziu@users.noreply.github.com>
2026-03-01 15:55:03 +0545

    works on windows now(maybe idk)

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # SMS
+
+## Compilation for Windows
+
+```bash
+
+gcc main.c student.c auth.c utils.c -o sms.exe
+sms.exe
+
+```
+
+## Compilation for Linux
+
+```bash
+
+make
+./sms
+
+```
--- utils.c
+++ utils.c
@@ -1,38 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+#ifdef _WIN32
+#include <conio.h>
+#include <windows.h>
+#else
 #include <termios.h>
 #include <unistd.h>
+#endif
+
 #include "utils.h"
 
 // Clears the Screen
 void clear_screen() {
-    system("clear");
+#ifdef _WIN32
+  system("cls");
+#else
+  system("clear");
+#endif
 }
 
 // Waits for users to use enter
 void wait_enter() {
-    printf("\nPress ENTER to continue...");
-    getchar();
-    getchar();
+  printf("\nPress ENTER to continue...");
+#ifdef _WIN32
+  getch();
+#else
+  getchar();
+  getchar();
+#endif
 }
 
-// Shows current time(linux only)
 void show_time() {
-    time_t now = time(NULL);
-    printf("%s\n", ctime(&now));
+  time_t now = time(NULL);
+  printf("%s\n", ctime(&now));
 }
 
 // Hide input(mainly used for password) by changing terminal behaviour
 void hidden_input(char *buffer) {
-    struct termios old, new; // Declare terminal settings
 
-    tcgetattr(STDIN_FILENO, &old); // Get current terminal settings
-    new = old; // Copy settings
-    new.c_lflag &= ~ECHO;  // Disable echo
-    tcsetattr(STDIN_FILENO, TCSANOW, &new); // Apply new settings
+#ifdef _WIN32
+  int i = 0;
+  char ch;
 
-    scanf("%s", buffer); // Read input
+  while ((ch = _getch()) != '\r') {
+    buffer[i++] = ch;
+    printf("*");
+  }
+  buffer[i] = '\0';
 
-    tcsetattr(STDIN_FILENO, TCSANOW, &old); // Restore original settings
+#else
+  struct termios old, new; // Declare terminal settings
+
+  tcgetattr(STDIN_FILENO, &old);          // Get current terminal settings
+  new = old;                              // Copy settings
+  new.c_lflag &= ~ECHO;                   // Disable echo
+  tcsetattr(STDIN_FILENO, TCSANOW, &new); // Apply new settings
+
+  scanf("%s", buffer); // Read input
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &old); // Restore original settings
+#endif
 }
